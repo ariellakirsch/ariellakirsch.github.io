3443c5e6adce5307ce368f7831464676de1fe0d4
Ariella <ariellakirsch@gmail.com>
2020-03-29 14:12:08 -0400

    ratatat

--- ratatat/ratatat.js
+++ ratatat/ratatat.js
@@ -1,11 +1,6 @@
 var cards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "9", "9", "9", "9", "9", "D", "D", "D", "P", "P", "P", "S", "S", "S"];
 var deck = new Array();
 
-function random() {
-    var x = Math.sin(seed++) * 10000;
-    return x - Math.floor(x);
-}
-
 function getDeck()
 {
   var deck = new Array();
@@ -28,11 +23,13 @@ function shuffle()
 
   // for 1000 turns
   // switch the values of two random cards
-  for (var i = 0; i < 1000; i++)
+  for (var i = 0; i < 10; i++)
   {
     var location1 = Math.floor(Math.random() * deck.length);
     var location2 = Math.floor(Math.random() * deck.length);
     var tmp = deck[location1];
+
+    console.log(location1+" "+location2);
 
     deck[location1] = deck[location2];
     deck[location2] = tmp;
