303af05c43dd7336ff537b3a1db31f804871b464
Ariella <ariellakirsch@gmail.com>
2020-03-29 12:27:19 -0400

    ratatat

--- ratatat/ratatat.js
+++ ratatat/ratatat.js
@@ -1,18 +1,14 @@
-var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-var suits = ["diamonds", "hearts", "spades", "clubs"];
+var cards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var deck = new Array();
 
 function getDeck()
 {
   var deck = new Array();
 
-  for(var i = 0; i < suits.length; i++)
+  for(var x = 0; x < cards.length; x++)
   {
-    for(var x = 0; x < cards.length; x++)
-    {
-      var card = {Value: cards[x], Suit: suits[i]};
-      deck.push(card);
-    }
+    var card = {Value: cards[x]};
+    deck.push(card);
   }
 
   return deck;
@@ -42,14 +38,11 @@ function renderDeck()
   {
     var card = document.createElement("div");
     var value = document.createElement("div");
-    var suit = document.createElement("div");
     card.className = "card";
     value.className = "value";
-    suit.className = "suit " + deck[i].Suit;
 
     value.innerHTML = deck[i].Value;
     card.appendChild(value);
-    card.appendChild(suit);
 
     document.getElementById("deck").appendChild(card);
   }
